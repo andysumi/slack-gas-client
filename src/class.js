16f7ad0a4cf1703b3eb735bc10a6c1c663f68d79
andysumi <andysumi@ruby.net.tokai-u.jp>
2018-07-12 14:20:32 +0900

    authTestメソッドを追加

--- src/class.js
+++ src/class.js
@@ -8,6 +8,23 @@
       if (!token) throw new Error('"token"は必須です');
     }
 
+    SlackClient.prototype.authTest = function() {
+      return this.fetch_('/auth.test', {'method': 'post'});
+    };
+
+    SlackClient.prototype.fetch_ = function(endPoint, options) {
+      var url = this.apiUrl + endPoint;
+      var response = UrlFetchApp.fetch(url, {
+        'method': options.method,
+        'muteHttpExceptions': true,
+        'contentType': 'application/json; charset=utf-8',
+        'headers': this.headers,
+        'payload': JSON.stringify(options.payload) || {}
+      });
+
+      return JSON.parse(response.getContentText());
+    };
+
     return SlackClient;
   })();
 
--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@
 function create(token) { // eslint-disable-line
   return new SlackClient(token);
 }
+
+/**
+ * APIの認証をチェックする
+ * https://api.slack.com/methods/auth.test
+ * @return {Object} 実行結果
+ */
+function authTest() { // eslint-disable-line
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
