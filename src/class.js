213556cb27c15d380451f79bd95fcaa3511af2bf
andysumi <andysumi@ruby.net.tokai-u.jp>
2018-08-01 13:29:47 +0900

    searchMessageメソッドを追加

--- src/class.js
+++ src/class.js
@@ -1,26 +1,37 @@
-(function(global) {
-  var SlackClient = (function() {
+(function (global) {
+  var SlackClient = (function () {
 
     function SlackClient(token) {
       this.apiUrl = 'https://slack.com/api';
-      this.headers = {'Authorization': 'Bearer ' + token};
+      this.headers = { 'Authorization': 'Bearer ' + token };
 
       if (!token) throw new Error('"token"は必須です');
     }
 
-    SlackClient.prototype.authTest = function() {
-      return this.fetch_(this.apiUrl + '/auth.test', {'method': 'post'});
+    SlackClient.prototype.authTest = function () {
+      return this.fetch_(this.apiUrl + '/auth.test', { 'method': 'post' });
     };
 
-    SlackClient.prototype.postMessageToUrl = function(url, params) {
-      return this.fetch_(url, {'method': 'post', 'payload': params});
+    SlackClient.prototype.postMessageToUrl = function (url, params) {
+      return this.fetch_(url, { 'method': 'post', 'payload': params });
     };
 
-    SlackClient.prototype.openDialog = function(triggerId, dialog) {
-      return this.fetch_(this.apiUrl + '/dialog.open', {'method': 'post', 'payload': {'trigger_id': triggerId, 'dialog': dialog}});
+    SlackClient.prototype.openDialog = function (triggerId, dialog) {
+      return this.fetch_(this.apiUrl + '/dialog.open', { 'method': 'post', 'payload': { 'trigger_id': triggerId, 'dialog': dialog } });
     };
 
-    SlackClient.prototype.fetch_ = function(url, options) {
+    SlackClient.prototype.searchMessage = function (query, options) {
+      var params = [];
+      if (options) {
+        for (var key in options) {
+          params.push(key + '=' + options[key]);
+        }
+        params = '&' + params.join('&');
+      }
+      return this.fetch_(this.apiUrl + '/search.messages?query=' + encodeURIComponent(query) + params, { 'method': 'get' });
+    };
+
+    SlackClient.prototype.fetch_ = function (url, options) {
       var response = UrlFetchApp.fetch(url, {
         'method': options.method,
         'muteHttpExceptions': true,
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * @param {String} token 【必須】APIアクセストークン
  * @return {SlackClient} Slack Clientのインスタンス
  */
-function create(token) { // eslint-disable-line
+function create(token) { // eslint-disable-line no-unused-vars
   return new SlackClient(token);
 }
 
@@ -12,7 +12,7 @@ function create(token) { // eslint-disable-line
  * https://api.slack.com/methods/auth.test
  * @return {Object} 実行結果
  */
-function authTest() { // eslint-disable-line
+function authTest() { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
 
@@ -22,7 +22,7 @@ function authTest() { // eslint-disable-line
  * @param {Object} params 【必須】メッセージの内容
  * @return {Object} 実行結果
  */
-function postMessageToUrl(url, params) { // eslint-disable-line
+function postMessageToUrl(url, params) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
 
@@ -33,6 +33,17 @@ function postMessageToUrl(url, params) { // eslint-disable-line
  * @param {Object} dialog 【必須】ダイアログの内容
  * @return {Object} 実行結果
  */
-function openDialog(triggerId, dialog) { // eslint-disable-line
+function openDialog(triggerId, dialog) { // eslint-disable-line no-unused-vars
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
+
+/**
+ * クエリに一致するメッセージを検索する
+ * https://api.slack.com/methods/search.messages
+ * @param {String} query 【必須】クエリ、検索条件
+ * @param {Object} options 【任意】オプション ※ドキュメント参照
+ * @return {Object} 実行結果
+ */
+function searchMessage(query, options) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
