b892752b1c287da5a75cdd3e74bc57020c16428a
Yoshiyuki Takeda <andysumi@ruby.net.tokai-u.jp>
2018-12-10 23:49:50 +0900

    Underscoreライブラリの導入

--- src/class.js
+++ src/class.js
@@ -1,3 +1,5 @@
+var _ = Underscore.load();
+
 (function (global) {
   var SlackClient = (function () {
 
@@ -16,11 +18,11 @@
       if (!channel) throw new Error('"channel"は必須です');
       if (!text) throw new Error('"text"は必須です');
 
-      var payload = { channel: channel, text: text };
-      for (var key in options) {
-        payload[key] = options[key];
-      }
-      return this.fetch_(Utilities.formatString('%s/chat.postMessage', this.apiUrl), { method: 'post', payload: payload });
+      if (!options) options = {};
+      return this.fetch_(Utilities.formatString('%s/chat.postMessage', this.apiUrl), { method: 'post', payload: _.extend({
+        channel: channel,
+        text:    text
+      }, options) });
     };
 
     SlackClient.prototype.postEphemeral = function (channel, text, userId, options) {
@@ -28,46 +30,49 @@
       if (!text) throw new Error('"text"は必須です');
       if (!userId) throw new Error('"userId"は必須です');
 
-      var payload = { channel: channel, text: text, user: userId };
-      for (var key in options) {
-        payload[key] = options[key];
-      }
-      return this.fetch_(Utilities.formatString('%s/chat.postEphemeral', this.apiUrl), { method: 'post', payload: payload });
+      if (!options) options = {};
+      return this.fetch_(Utilities.formatString('%s/chat.postEphemeral', this.apiUrl), { method: 'post', payload: _.extend({
+        channel: channel,
+        text:    text,
+        user:    userId
+      }, options) });
     };
 
-    SlackClient.prototype.updateMessage = function (channel, text, timestamp, options) {
-      if (!channel) throw new Error('"channel"は必須です');
+    SlackClient.prototype.updateMessage = function (channelId, text, timestamp, options) {
+      if (!channelId) throw new Error('"channelId"は必須です');
       if (!text) throw new Error('"text"は必須です');
       if (!timestamp) throw new Error('"timestamp"は必須です');
 
-      var payload = { channel: channel, text: text, ts: timestamp };
-      for (var key in options) {
-        payload[key] = options[key];
-      }
-      return this.fetch_(Utilities.formatString('%s/chat.update', this.apiUrl), { method: 'post', payload: payload });
+      if (!options) options = {};
+      return this.fetch_(Utilities.formatString('%s/chat.update', this.apiUrl), { method: 'post', payload: _.extend({
+        channel: channelId,
+        text:    text,
+        ts:      timestamp
+      }, options) });
     };
 
-    SlackClient.prototype.deleteMessage = function (channel, timestamp, options) {
-      if (!channel) throw new Error('"channel"は必須です');
+    SlackClient.prototype.deleteMessage = function (channelId, timestamp, options) {
+      if (!channelId) throw new Error('"channelId"は必須です');
       if (!timestamp) throw new Error('"timestamp"は必須です');
 
-      var payload = { channel: channel, ts: timestamp };
-      for (var key in options) {
-        payload[key] = options[key];
-      }
-      return this.fetch_(Utilities.formatString('%s/chat.delete', this.apiUrl), { method: 'post', payload: payload });
+      if (!options) options = {};
+      return this.fetch_(Utilities.formatString('%s/chat.delete', this.apiUrl), { method: 'post', payload: _.extend({
+        channel: channelId,
+        ts:      timestamp
+      }, options) });
     };
 
-    SlackClient.prototype.unfurlUrl = function (channel, timestamp, unfurls, options) {
-      if (!channel) throw new Error('"channel"は必須です');
+    SlackClient.prototype.unfurlUrl = function (channelId, timestamp, unfurls, options) {
+      if (!channelId) throw new Error('"channel"は必須です');
       if (!timestamp) throw new Error('"timestamp"は必須です');
       if (!unfurls) throw new Error('"unfurls"は必須です');
 
-      var payload = { channel: channel, ts: timestamp, unfurls: unfurls };
-      for (var key in options) {
-        payload[key] = options[key];
-      }
-      return this.fetch_(Utilities.formatString('%s/chat.unfurl', this.apiUrl), { method: 'post', payload: payload });
+      if (!options) options = {};
+      return this.fetch_(Utilities.formatString('%s/chat.unfurl', this.apiUrl), { method: 'post', payload: _.extend({
+        channel: channelId,
+        ts:      timestamp,
+        unfurls: unfurls
+      }, options) });
     };
 
     SlackClient.prototype.postMessageToUrl = function (url, params) {
@@ -81,17 +86,17 @@
       if (!triggerId) throw new Error('"triggerId"は必須です');
       if (!dialog) throw new Error('"dialog"は必須です');
 
-      return this.fetch_(Utilities.formatString('%s/dialog.open', this.apiUrl), { method: 'post', payload: { trigger_id: triggerId, dialog: dialog } });
+      return this.fetch_(Utilities.formatString('%s/dialog.open', this.apiUrl), { method: 'post', payload: {
+        trigger_id: triggerId,
+        dialog: dialog
+      } });
     };
 
     SlackClient.prototype.searchMessages = function (query, options) {
       if (!query) throw new Error('"query"は必須です');
 
-      var params = { query: query };
-      for (var key in options) {
-        params[key] = options[key];
-      }
-      params = this.buildUrlParam_(params);
+      if (!options) options = {};
+      var params = this.buildUrlParam_(_.extend({ query: query }, options));
       return this.fetch_(Utilities.formatString('%s/search.messages?%s', this.apiUrl, params), { method: 'get' });
     };
 
--- src/main.js
+++ src/main.js
@@ -44,38 +44,38 @@ function postEphemeral(channel, text, userId, options) { // eslint-disable-line 
 /**
  * メッセージを更新する
  * https://api.slack.com/methods/chat.update
- * @param {String} channel 【必須】更新するメッセージが送信されたChannelのID もしくは名前
+ * @param {String} channelId 【必須】更新するメッセージが送信されたChannelのID
  * @param {String} text 【必須】メッセージのテキスト
  * @param {String} timestamp 【必須】更新するメッセージのタイムスタンプ
  * @param {Object} options 【任意】オプション ※APIドキュメント参照
  * @return {Object} 実行結果
  */
-function updateMessage(channel, text, timestamp, options) { // eslint-disable-line no-unused-vars
+function updateMessage(channelId, text, timestamp, options) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
 
 /**
  * メッセージを削除する
  * https://api.slack.com/methods/chat.delete
- * @param {String} channel 【必須】削除するメッセージが送信されたChannelのID もしくは名前
+ * @param {String} channelId 【必須】削除するメッセージが送信されたChannelのID
  * @param {String} timestamp 【必須】削除するメッセージのタイムスタンプ
  * @param {Object} options 【任意】オプション ※APIドキュメント参照
  * @return {Object} 実行結果
  */
-function deleteMessage(channel, timestamp, options) { // eslint-disable-line no-unused-vars
+function deleteMessage(channelId, timestamp, options) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
 
 /**
  * ユーザーが投稿したURLを展開する
  * https://api.slack.com/methods/chat.unfurl
- * @param {String} channel 【必須】メッセージが送信されたChannelのID もしくは名前
+ * @param {String} channelId 【必須】メッセージが送信されたChannelのID
  * @param {String} timestamp 【必須】メッセージのタイムスタンプ
  * @param {Object} unfurls 【必須】展開するメッセージの内容(展開するURLをキーとする)
  * @param {Object} options 【任意】オプション ※APIドキュメント参照
  * @return {Object} 実行結果
  */
-function unfurlUrl(channel, timestamp, unfurls, options) { // eslint-disable-line no-unused-vars
+function unfurlUrl(channelId, timestamp, unfurls, options) { // eslint-disable-line no-unused-vars
   throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
 }
 
